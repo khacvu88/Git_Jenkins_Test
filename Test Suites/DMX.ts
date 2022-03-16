f671b9f09bc627613f0e207862310a910018d2cd
Vu Nguyen Khac <nkhacvu@tma.com.vn>
2022-03-16 11:15:17 +0700

    first commit

---
+++ Test Suites/DMX.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DMX</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f8298b91-3f8e-4dc4-9df3-6da609ff7f7a</testSuiteGuid>
+   <testCaseLink>
+      <guid>29e798c7-f867-4c02-8149-45421ebf6d5d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/dienmayxanh</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
